a04246e2616210e679e72b3cbcb65c393cc6beb4
Dominik Fistric <dominik.fistric@gmail.com>
2022-05-28 17:27:05 +0200

    task vinjete added

---
+++ hio/vinjete/fistric_2subtask.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define ll long long
+
+using namespace std;
+
+struct edge {
+  int y;
+  int a, b;
+};
+
+const int OFF = (1 << 17);
+vector <edge> ve[OFF];
+
+multiset <pair <int, int> > se;
+int sol[OFF];
+
+void dfs(int x, int par) {
+  int val = 0, la = 0;
+  for (auto tr : se) {
+    if (tr.first != la) {
+      sol[x] += (tr.first - la) * (val > 0);
+      la = tr.first;
+    }
+    if (tr.second == 0) val++;
+    else                val--;
+  }
+
+  for (auto tr : ve[x]) {
+    if (tr.y == par) continue;
+
+    se.insert({tr.a, 0});
+    se.insert({tr.b + 1, 1});
+
+    dfs(tr.y, x);
+
+    se.erase(se.find({tr.a, 0}));
+    se.erase(se.find({tr.b + 1, 1}));
+  }
+
+  return;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  int n;
+  cin >> n;
+  REP(i, n - 1) {
+    int x, y, a, b;
+    cin >> x >> y >> a >> b;
+    x--, y--;
+    ve[x].push_back({y, a, b});
+    ve[y].push_back({x, a, b});
+  }
+
+  dfs(0, 0);
+  REP(i, n) cout << sol[i] << " ";
+
+  return 0;
+}
---
+++ hio/vinjete/vinjete_pravo.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define REP(i, n) FOR(i, 0, n)
+#define ll long long
+
+using namespace std;
+
+struct edge {
+  int y;
+  int a, b;
+};
+
+struct node {
+  int val, sum;
+  node *l, *r;
+
+  node() {val = 0, sum = 0, l = r = NULL;}
+};
+
+const int OFF = (1 << 30);
+const int MAXN = (1 << 17);
+vector <node*> roots;
+
+void update(int a, int b, int lo, int hi, node *x, node *y) {
+  if (y->val != 0) {
+    x->val = y->val;
+    x->sum = x->val;
+    return;
+  }
+  //cout << "!! " << a << " " << b << " " << lo << " " << hi << " " << x << " " << y << endl;
+  if (b <= lo || hi <= a) return;
+  if (a <= lo && hi <= b) {
+    x->val = (hi - lo);
+    x->sum = x->val;
+    return;
+  }
+
+  if (y->l == NULL) y->l = new node;
+  if (y->r == NULL) y->r = new node;
+  if (x->l == NULL) x->l = new node;
+  if (x->r == NULL) x->r = new node;
+
+  int mi = (lo + hi) / 2;
+
+  if (mi <= a) {
+    x->l = y->l;
+  } else {
+    update(a, b, lo, mi, x->l, y->l);
+  }
+
+  if (b <= mi) {
+    x->r = y->r;
+  } else {
+    update(a, b, mi, hi, x->r, y->r);
+  }
+
+  x->sum = x->l->sum + x->r->sum;
+
+  return;
+}
+
+int sol[MAXN];
+vector <edge> ve[MAXN];
+
+void print(node* x) {
+  if (x == NULL) return;
+  cout << x << " " << x->val << " " << x->sum << " " << x->l << " " << x->r << endl;
+  print(x->l);
+  print(x->r);
+}
+
+void dfs(int x, int par) {
+  sol[x] = roots[x]->sum;
+
+  for (auto tr : ve[x]) {
+    if (tr.y == par) continue;
+
+    //cout << "\n.....\n";
+    //print(roots[x]);
+    //cout << "....\n";
+    //print(roots[tr.y]);
+    //cout << "....\n";
+    update(tr.a - 1, tr.b, 0, OFF, roots[tr.y], roots[x]);
+
+    //print(roots[x]);
+    //cout << "....\n";
+    //print(roots[tr.y]);
+    //cout << "....\n";
+    dfs(tr.y, x);
+  }
+
+  return;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+
+  int n;
+  cin >> n;
+  REP(i, n - 1) {
+    int x, y, a, b;
+    cin >> x >> y >> a >> b;
+    x--, y--;
+    ve[x].push_back({y, a, b});
+    ve[y].push_back({x, a, b});
+  }
+
+  REP(i, n) roots.push_back(new node);
+  dfs(0, 0);
+  REP(i, n) cout << sol[i] << " ";
+
+  return 0;
+}
