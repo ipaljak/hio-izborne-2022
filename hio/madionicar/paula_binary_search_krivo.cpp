ef715d40e480636ec7d527b8f401aa86ef689520
Paula Vidas <paulavidas@gmail.com>
2022-05-29 13:18:25 +0200

    madionicar editorial

---
+++ hio/madionicar/paula_binary_search_krivo.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define TRACE(x) cerr << __LINE__ << ": " << #x << " = " << x << endl
+#define _ << " _ " <<
+
+template<class> struct is_container : false_type {};
+template<class... Ts> struct is_container<vector<Ts...>> : true_type {};
+template<class... Ts> struct is_container<map<Ts...>> : true_type {};
+template<class... Ts> struct is_container<unordered_map<Ts...>> : true_type {};
+template<class... Ts> struct is_container<set<Ts...>> : true_type {};
+template<class... Ts> struct is_container<unordered_set<Ts...>> : true_type {};
+template<class... Ts> struct is_container<multiset<Ts...>> : true_type {};
+template<class... Ts> struct is_container<unordered_multiset<Ts...>> : true_type {};
+template<class T, class = typename enable_if<is_container<T>::value>::type>
+ostream& operator<<(ostream &o, T x) {
+  int f = 1;
+  o << "{";
+  for (auto y : x) {
+    o << (f ? "" : ", ") << y;
+    f = 0;
+  }
+  return o << "}";
+}
+template<class T, class U>
+ostream& operator<<(ostream &o, pair<T, U> x) {
+  return o << "(" << x.first << ", " << x.second << ")";
+}
+
+#define fi first
+#define se second
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+bool query(int l, int r) {
+    cout << "? " << l << " " << r << endl;
+    bool ans;
+    cin >> ans;
+    return ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int lo = 1, hi = n + 1;
+    while (lo + 1 < hi) {
+        int mid = (lo + hi) / 2;
+        bool found = false;
+        for (int i = 1; i + mid <= n + 1 && !found; i++)
+            found |= query(i, i + mid - 1);
+        if (found) lo = mid;
+        else hi = mid;
+    }
+
+    cout << "! " << lo << endl;
+
+    return 0;
+}
+
